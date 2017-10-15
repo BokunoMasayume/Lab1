158f7b366228a0ffd95a56514f3ac2b6e02525a2
BokunoMasayume <BokunoMasayume@163.com>
2017-10-16 01:17:31 +0800

    modify 4 files in B1

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
 sdsadasfadvafadvfbvbssk
-=======
+//one of 4 modify files
 **********c4 changes 3******************
 
--- src/main/java/test/tttest/TestListG.java
+++ src/main/java/test/tttest/TestListG.java
@@ -2,7 +2,7 @@ package test.tttest;
 
 import java.util.Random;
 import java.io.*;
-/*********B1 may conf change*******/
+//one of 4 modify files
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
